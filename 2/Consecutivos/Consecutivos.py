fa46b65c22efac59d0ce3a703f1fa30afe5ccc75
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-10-26 17:25:46 -0300

    Create Consecutivos.py

---
+++ 2/Consecutivos/Consecutivos.py
@@ -0,0 +1,18 @@
+def main():
+    A = int(input())
+    B = input()
+    lista = [int(n) for n in B.split()]
+    consecutive = 0
+    max = 0
+    for n in range(len(lista) - 1):
+        if lista[n] == lista[n + 1]:
+            consecutive += 1 
+        else:
+            consecutive += 1 
+            if consecutive > max:
+                max = consecutive
+            consecutive = 0
+    print(max)
+
+if __name__ == "__main__":
+    main()
