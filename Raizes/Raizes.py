0af2b2bad36fd9e1beff343988716d502e0c2aeb
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-10-26 16:04:20 -0300

    Update Raizes.py

--- Raizes/Raizes.py
+++ Raizes/Raizes.py
@@ -1,1 +1,8 @@
-R
+def main():
+    A = int(input())
+    B = input()
+    lista = [float(n) for n in B.split()]
+    for n in lista:
+        print("{:.4f}".format(n ** (1/2)))
+if __name__ == "__main__":
+    main()
