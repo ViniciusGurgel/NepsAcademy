38658a6e2786af207466fd2f478e92d8fef97af2
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-10-26 16:36:15 -0300

    Update Vestibular.py

--- 2/Vestibular/Vestibular.py
+++ 2/Vestibular/Vestibular.py
@@ -1,1 +1,13 @@
-
+def main():
+    A = int(input())
+    B = input()
+    C = input()
+    acertos = 0
+    index = 0
+    for char in B:
+        if char == C[index]:
+            acertos += 1 
+        index += 1
+    print(acertos)
+if __name__ == "__main__":
+    main()
