0604c2529ec8cb60e41ebcd02a1a328ce57a2f62
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-22 14:29:05 -0300

    Update Gangorra.py

--- 1/Gangorra/Gangorra.py
+++ 1/Gangorra/Gangorra.py
@@ -1,4 +1,8 @@
-# Lendo a entrada do exercício
-P1, C1, P2, C2 = map(int, input().split())
-print("0" if P1*C1 == P2*C2 else "1")
-# Seu código vai aqui
+def main():
+    P1, C1, P2, C2 = map(int, input().split())
+    produto_esquerda = P1 * C1
+    produto_direita = P2 * C2
+    print(0 if produto_esquerda == produto_direita else (1 if produto_esquerda < produto_direita else -1))
+
+if __name__ == "__main__":
+    main()
