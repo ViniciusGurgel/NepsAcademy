a250b9b4fc0bd6c5096682970b32eea42cd2019f
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-28 16:48:02 -0300

    Delete 5/Plantação de Morangos/Morangos.py

--- 5/Plantação de Morangos/Morangos.py
+++
@@ -1,9 +0,0 @@
-def main():
-    C1 = int(input())
-    L1 = int(input())
-    C2 = int(input())
-    L2 = int(input())
-    print(C1*L1 if C1*L1>C2*L2 else C2*L2)
-
-if __name__ == "__main__":
-    main()
