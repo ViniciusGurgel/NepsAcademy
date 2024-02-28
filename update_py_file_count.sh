b5b181a253e45e8474d4fe94b5941dc04f15c679
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-28 17:38:42 -0300

    Update update_py_file_count.sh

--- update_py_file_count.sh
+++ update_py_file_count.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+# Count the number of Python files
 python_file_count=$(find . -type f -name '*.py' | wc -l)
-sed -i "s/{{ PYTHON_FILE_COUNT }}/$python_file_count/g" README.md
 
+# Count the number of Java files
 java_file_count=$(find . -type f -name '*.java' | wc -l)
-sed -i "s/{{ JAVA_FILE_COUNT }}/$java_file_count/g" README.md
 
+# Count the number of C files
 c_file_count=$(find . -type f -name '*.c' | wc -l)
+
+# Replace the placeholders in README.md with the actual file counts
+sed -i "s/{{ PYTHON_FILE_COUNT }}/$python_file_count/g" README.md
+sed -i "s/{{ JAVA_FILE_COUNT }}/$java_file_count/g" README.md
 sed -i "s/{{ C_FILE_COUNT }}/$c_file_count/g" README.md
+
