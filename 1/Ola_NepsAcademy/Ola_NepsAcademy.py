be27863547f7426ae929e08b622b1a712896d182
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-29 16:48:01 -0300

    Delete 1/Ola_NepsAcademy directory

--- 1/Ola_NepsAcademy/Ola_NepsAcademy.py
+++
@@ -1,2 +0,0 @@
-# Já que esse é um exercício especial nós já resolvemos ele pra você :). Porém, preste atenção como a saída precisa ser EXATAMENTE como é pedido no enunciado do exercício.
-print("Ola Neps Academy!")
