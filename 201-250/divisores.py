81f792f2c602ef91a90ed0e9f69ba99afd186ffc
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-29 17:23:00 -0300

    Delete 201-250/divisores.py

--- 201-250/divisores.py
+++
@@ -1,8 +0,0 @@
-def main():
-    A = int(input())
-    lista = [n for n in range(1,A) if A % n == 0]
-    lista.append(A)
-    print(' '.join(map(str,lista)))
-
-if __name__ == "__main__":
-    main()
