fad7317bc69f0c4466092ef16a212f46e445b71a
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-29 15:48:46 -0300

    Update primo.py

--- 5/Função_Primo/primo.py
+++ 5/Função_Primo/primo.py
@@ -1,11 +1,19 @@
 def eh_primo(x):
-    for i in range(2,x):
+    if x <= 1:
+        return False
+    if x == 2:
+        return True
+    if x % 2 == 0:
+        return False
+    for i in range(3, int(x**0.5) + 1, 2):
         if x % i == 0:
             return False
     return True
 
 x = int(input())
 if eh_primo(x):
-	print('S')
+    print('S')
 else:
-	print('N')
+    print('N')
+
+
