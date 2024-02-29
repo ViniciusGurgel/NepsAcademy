d171c0628277f70f36729f50f76e8451cc33a9b9
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-02-29 16:45:43 -0300

    Create Café.py

---
+++ 51-100/95/Café.py
@@ -0,0 +1,9 @@
+def main():
+    A1 = int(input())
+    A2 = int(input())
+    A3 = int(input())
+    lista = [A2*2 + A3*4,A1*2 + A3*2,A1*4 + A2*2]
+    print(min(lista))
+    
+if __name__ == "__main__":
+    main()
