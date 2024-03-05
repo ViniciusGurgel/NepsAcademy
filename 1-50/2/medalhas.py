d9257bc50005674bcd0a2dea8ef398c0497b599e
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-03-05 15:46:24 -0300

    Update medalhas.py

--- 1-50/2/medalhas.py
+++ 1-50/2/medalhas.py
@@ -1,8 +1,5 @@
 def main():
-    A = int(input())
-    B = int(input())
-    C = int(input())
-    lista = [A, B, C]
+    lista = [int(input())for i in range(3)]
     primeiro = lista.index(min(lista))+1
     terceiro = lista.index(max(lista))+1
     lista[lista.index(min(lista))] = 0
