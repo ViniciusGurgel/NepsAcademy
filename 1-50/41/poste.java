152ac5a460e7036d5a95e6683f23521f1a1c7f15
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-05-17 15:26:21 -0300

    Create poste.java

---
+++ 1-50/41/poste.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int N = Integer.parseInt(obj.nextLine());
+        int[] lista = new int[5];
+        for (int i = 0; i < N; i++) {
+            lista[i] = obj.nextInt();
+        }
+
+        int count = 0;
+        int count1 = 0;
+        for(int i = 0;i < N;i++){
+            if(lista[i] < 50){
+                count++;
+            }else if(lista[i] >= 85){
+                continue;
+            }else{
+                count1++;
+            }
+        }
+
+        System.out.println(count + " " + count1);
+        obj.close();
+    }
+}
