5d0a08476c51a1bfc8b25b6ccc47a36e0328cfbd
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2024-05-17 13:49:21 -0300

    Create game-10.java

---
+++ 1-50/17/game-10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int N = obj.nextInt();
+        int D = obj.nextInt();
+        int A = obj.nextInt();
+        int result = (A < D) ? D - A : D + N - A;
+        System.out.println(result);
+        obj.close();
+    }
+}
